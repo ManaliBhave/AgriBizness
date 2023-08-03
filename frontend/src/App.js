25ef1d1b6edc07456e938606d09e9f93c1f2fc45
Soham Hajare <soham.hajare2003@gmail.com>
2023-08-03 21:14:52 +0530

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,26 @@
-import Navbar from "./components/Navbar";
+import logo from './logo.svg';
+import './App.css';
+//import Form from './Components/Form';
+import Form2 from './Components/Form2';
+import img1 from './Images/RICE.jpg';
+
+// import Navbar from './Components/Navbar'
 function App() {
   return (
-    <div className="App text-4xl font-bold underline">
-      <Navbar/>
+    <div>
+      {/* <Navbar /> */}
+      
+      
+      
+      {/* <img src={img1} alt="" width={1265} height={600} /> */}
+      
+      
+      {/* <Form/> */}
+      <Form2/>
+      
+      
+      
+      
     </div>
   );
 }
